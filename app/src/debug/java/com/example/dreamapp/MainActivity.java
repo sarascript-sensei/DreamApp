a0f5c8a583d13e5484584bcdbea51ce309e2b482
sarascript-sensei <sara.2000year14@gmail.com>
2020-07-20 15:17:42 +0600

    Check out

--- app/src/debug/java/com/example/dreamapp/MainActivity.java
+++ app/src/debug/java/com/example/dreamapp/MainActivity.java
@@ -1,14 +1,115 @@
 package com.example.dreamapp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+
+import com.example.dreamapp.Models.User;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class MainActivity extends AppCompatActivity {
+    Button btnSignIn, btnRegister;
+    FirebaseAuth auth;
+    FirebaseDatabase db;
+    DatabaseReference users;
 
+    RelativeLayout root;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSignIn= findViewById(R.id.btnSingIn);
+        btnRegister= findViewById(R.id.btnRegister);
+
+        root=findViewById(R.id.root_element);
+        auth = FirebaseAuth.getInstance();
+        db = FirebaseDatabase.getInstance();
+        users = db.getReference("Users");
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showRegisterWindow ();
+            }
+
+        });
+    }
+    private void showRegisterWindow() {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Зарегистрироваться");
+        dialog.setMessage("Введите все данные для регистрации");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View register_window = inflater.inflate(R.layout.register_window, null);
+        dialog.setView(register_window);
+        final MaterialEditText name = register_window.findViewById(R.id.nameField);
+        final MaterialEditText phone = register_window.findViewById(R.id.phoneField);
+        final MaterialEditText email = register_window.findViewById(R.id.emailField);
+        final MaterialEditText pass = register_window.findViewById(R.id.passField);
+
+        dialog.setNegativeButton("Отменить", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        dialog.setPositiveButton("Добавить", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                if(TextUtils.isEmpty(name.getText().toString())){
+                    Snackbar.make(root, "Введите ваше имя", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(phone.getText().toString())){
+                    Snackbar.make(root, "Введите ваш телефон", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(email.getText().toString())){
+                    Snackbar.make(root, "Введите вашу почту", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if(pass.getText().toString().length() < 8){
+                    Snackbar.make(root, "Введите пароль более 8 символов", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                //Регистрация пользователя
+                auth.createUserWithEmailAndPassword(email.getText().toString(), pass.getText().toString())
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                User user = new User();
+                                user.setName(name.getText().toString());
+                                user.setPhone(phone.getText().toString());
+                                user.setEmail(email.getText().toString());
+                                user.setPass(pass.getText().toString());
+
+                                users.child(user.getEmail())
+                                        .setValue(user)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Snackbar.make(root, "Пользователь добавлен!", Snackbar.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+                        });
+            }
+        });
+
+        dialog.show();
     }
 }
